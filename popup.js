490fd78750c4f0a2b3d08170b495bcc187b798c4
shivam bansal <shivamb61@gmail.com>
2016-08-09 22:45:41 +0530

    working

--- popup.js
+++ popup.js
@@ -5,10 +5,15 @@ app.controller('cricket',function($scope,$http){
 	        $scope.show=function(){
 				var playerId=$scope.name;
 				 $http.get('http://cricapi.com/api/playerStats?pid='+playerId)
-	           .success(function(response){
+	            .success(function(response){
 		            $scope.first=response.name;
+					$scope.second=response.batting;
+					$scope.third=response.bowling;
+					$scope.fifth=response.country;
+					$scope.fourth=response.born;
+					
 	  });//10582
 			}
 	  });
 	 
-	  
+	
